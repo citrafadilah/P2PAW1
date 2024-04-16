48e31a45463b291152c9824d7bce9db5d18d8975
citrafadilah <citrafadilahhasibuan_2226240089@mhs.mdp.ac.id>
2024-04-16 16:58:46 +0700

    update latihan3

---
+++ latihan3/script.js
@@ -0,0 +1,46 @@
+let inputnama = document.getElementById("nama");
+let inputtestimoni = document.getElementById("testimoni");
+let btnSimpan = document.getElementById("btnSimpan");
+let list = document.getElementById("list");
+let data = [];
+
+btnSimpan.addEventListener("click", function() {
+    if (localStorage.getItem("testimoni")) {
+        // ada localStorage dengan key testimoni
+        data = JSON.parse(localStorage.getItem("testimoni"));
+        data.push({
+            nama: inputnama.value,
+            testimoni: inputtestimoni.value,
+        });
+        console.log(data);
+    } else {
+        // belum ada localStorage dengan key testimoni
+        data.push({
+            nama: inputnama.value,
+            testimoni: inputtestimoni.value,
+        });
+        console.log(data);
+    } 
+    // simpan ke dalam localStorage
+    localStorage.setItem("testimoni", JSON.stringify(data));
+    // panggil getData()
+    getData();
+});
+
+// tampilkan data localStorage
+function getData() {
+    // clear element list
+    list.innerHTML = "";
+
+    if (localStorage.getItem("testimoni")) {
+        // ada localStorage dengan key testimoni
+        data = JSON.parse(localStorage.getItem("testimoni"));
+        for (const [index, row] of data.entries()) {
+            // list.innerHTML += `<li${row.nama} ${row.testimoni}</li>`;
+            list.innerHTML += `<tr><td>${row.nama}</td><td>${row.testimoni}</td></tr>`;
+        }
+    }
+}
+
+// panggil getData()
+getData();
